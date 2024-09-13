4dba1ca76a2e80d8b3eebc3f8f10f15571668d76
Nathan Lima <nathanls.dev@gmail.com>
2024-09-13 12:20:54 -0300

    refactor: createElements permite criar elementos repetidos

--- assets/js/utils/createElements.js
+++ assets/js/utils/createElements.js
@@ -1,8 +1,11 @@
-const createElements = (...elementNames) => {
+const createElements = (elements) => {
   const createdElements = {};
 
-  elementNames.forEach((elementName) => {
-    createdElements[elementName] = document.createElement(elementName);
+  Object.keys(elements).forEach((elementName) => {
+    createdElements[elementName] = [];
+    for (let i = 0; i < elements[elementName]; i++) {
+      createdElements[elementName].push(document.createElement(elementName));
+    }
   });
 
   return createdElements;
