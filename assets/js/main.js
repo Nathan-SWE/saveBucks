07815210dbf82f106b8fa4aca315debea0fcfb10
Nathan Lima <nathanls.dev@gmail.com>
2024-09-13 16:02:08 -0300

    refactor: invoca somente componente presentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,2 @@
 import { presentationComponent } from "./components/presentationComponent.js";
-import { loginComponent } from "./components/loginComponent.js";
-
 presentationComponent();
-loginComponent();
