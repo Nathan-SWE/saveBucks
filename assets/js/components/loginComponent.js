5f3efc304273bc081d89394f0f05cdac45bf82b9
Nathan Lima <nathanls.dev@gmail.com>
2024-09-14 00:08:27 -0300

    refactor: define a geracao de forms para func auxiliares

--- assets/js/components/loginComponent.js
+++ assets/js/components/loginComponent.js
@@ -1,74 +1,36 @@
 import { createElements } from "../utils/createElements.js";
+import { handleForms } from "../utils/formsHandler.js";
+import { signInForm } from "./signInFormComponent.js";
 
 const loginComponent = () => {
   const body = document.body;
-  const fragment = document.createDocumentFragment();
-
-  const eyeStatus = "../../../public/svg/eye-password-hide.svg";
-  const googleIcon = "../../../public/svg/google-original.svg";
-  const fbIcon = "../../../public/svg/facebook-original.svg";
-  const githubIcon = "../../../public/svg/github-original.svg";
 
   const { section } = createElements({
     section: 1,
   });
 
   const loginSection = section[0];
-  loginSection.classList.add("login")
+  loginSection.classList.add("login");
 
   loginSection.innerHTML = `
-      <header>
-        <h1>SaveBucks</h1>
-      </header>
-
-      <form action="" class="form">
-        <div class="actions">
-          <button>login</button>
-          <button>Cadastrar</button>
-        </div>
-
-        <div class="user-inputs">
-          <label for="signInEmail">E-mail:</label>
-          <input
-            type="email"
-            name="email"
-            id="signInEmail"
-            placeholder="example@email.com"
-          />
-          <button class="test-user">?</button>
-
-          <label for="signInPassword">Senha:</label>
-          <input type="password" name="password" id="signInPassword" />
-          <button class="password-hidden">
-            <img
-              src=${eyeStatus}
-              alt="hide-password"
-            />
-          </button>
-          <a href="#">Esqueceu a senha?</a>
-
-          <input class="submit button" type="submit" value="Entrar" />
-        </div>
-
-        <fieldset class="social-auth">
-          <p>ou</p>
-          <div>
-            <span class="button">
-              <img src=${googleIcon} alt="google-logo" />
-            </span>
-            <span class="button">
-              <img src=${fbIcon} alt="facebook-logo" />
-            </span>
-            <span class="button">
-              <img src=${githubIcon} alt="github-logo" />
-            </span>
-          </div>
-        </fieldset>
-      </form>
+    <header>
+      <h1>SaveBucks</h1>
+    </header>
+    
+    <form action="" class="form">
+      <div class="actions" data-js="authActions">
+        <button data-js="signInBtn">Login</button>
+        <button data-js="signUpBtn">Cadastrar</button>
+      </div>
+    </form>
 `;
 
-  fragment.appendChild(loginSection);
-  body.appendChild(fragment);
+  const form = loginSection.querySelector("form");
+
+  form.appendChild(signInForm());
+  body.appendChild(loginSection);
+
+  handleForms(form);
 };
 
 export { loginComponent };
